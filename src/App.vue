6217397a5edad36b99ed88cf37f8ef8de9351367
thejacer87 <jacebennest87@gmail.com>
2022-08-22 07:53:22 -0700

    pin solana version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
-import { Program, Provider, web3 } from '@project-serum/anchor';
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  import HelloWorld from './components/HelloWorld.vue';
+  import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+  import { Program, Provider, web3 } from '@project-serum/anchor';
 </script>
 
 <template>
@@ -16,15 +19,15 @@ import { Program, Provider, web3 } from '@project-serum/anchor';
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
